1afb5912072e38c0c7f207bf558af853041443ac
sankethacr <sanketha.cr@reltio.com>
2020-08-07 18:21:49 +0530

    adding files

---
+++ discovery-server/src/test/java/com/java/brain/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.brain.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
