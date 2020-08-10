d1a5bd34920afacb8d69c2ba7fd1f40820bc0228
sankethacr <sanketha.cr@reltio.com>
2020-08-10 17:42:03 +0530

    Updated 10-08-2020

--- spring-boot-config/src/main/java/com/java/brain/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/java/brain/springbootconfig/GreetingController.java
@@ -1,10 +1,10 @@
 package com.java.brain.springbootconfig;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,26 +13,37 @@ import com.java.brain.springbootconfig.model.DBSettings;
 @RestController
 public class GreetingController {
 
-	//If my.greeting value does not exist it takes "Default Value" string
+	// If my.greeting value does not exist it takes "Default Value" string
 	@Value("${my.greeting:Default value}")
 	private String greet;
-	
+
 	@Value("Static value")
 	private String staticValue;
 
-	@Value("${my.list.values}")
+	@Value("${my.list.values: Default list}")
 	private List<String> list;
-	
+
 	// # Spring expression language
-	@Value("#{${my.dbvalues}}")
-	private Map<Object,Object> dbconnection;
-	
+	/*
+	 * @Value("#{${my.db.values}}") private Map<Object,Object> dbconnection;
+	 */
+
 	@Autowired
 	private DBSettings dbsettings;
-	
+
+	@Autowired
+	private Environment env;
+
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greet +" "+staticValue+" "+list+" "+dbconnection+" "+dbsettings.getHost();
+		return greet + " " + staticValue + " " + dbsettings.getHost();
+	}
+
+	@GetMapping("/envdetails")
+	public String enDetails() {
+		
+		return env.getProperty("propertySources");
+		//return env.toString();
 	}
 
 }
