8f6365d670cc5dcd7890b6403b8aee62600f32b0
sankethacr <sanketha.cr@reltio.com>
2020-08-19 18:58:52 +0530

    Updated config

--- spring-boot-config/src/main/java/com/java/brain/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/java/brain/springbootconfig/GreetingController.java
@@ -1,49 +1,51 @@
 package com.java.brain.springbootconfig;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.java.brain.springbootconfig.model.DBSettings;
-
 @RestController
+@RefreshScope // Refreshe config values from Spring cloud config server
 public class GreetingController {
 
 	// If my.greeting value does not exist it takes "Default Value" string
-	@Value("${my.greeting:Default value}")
+	@Value("${my.greeting}")
 	private String greet;
+
+	/*
+	 * @Value("${app.Description}") private String applicationDes;
+	 */
 
 	@Value("Static value")
 	private String staticValue;
 
-	@Value("${my.list.values: Default list}")
-	private List<String> list;
+	/*
+	 * @Value("${list.values}") private List<String> list;
+	 */
 
 	// # Spring expression language
 	/*
 	 * @Value("#{${my.db.values}}") private Map<Object,Object> dbconnection;
 	 */
 
-	@Autowired
-	private DBSettings dbsettings;
-
-	@Autowired
-	private Environment env;
+	/*
+	 * @Value("${my.list.values}") private List<String> mylist;
+	 */
+	/*
+	 * @Autowired private DBSettings dbsettings;
+	 * 
+	 * @Autowired private Environment env;
+	 */
 
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greet + " " + staticValue + " " + dbsettings.getHost();
+		return greet + " " + staticValue;
 	}
 
-	@GetMapping("/envdetails")
-	public String enDetails() {
-		
-		//return env.getProperty("propertySources");
-		return env.toString();
-	}
-
+//	/*
+//	 * @GetMapping("/envdetails") public String enDetails() {
+//	 * 
+//	 * // return env.getProperty("propertySources"); return env.toString(); }
+//	 */
 }
